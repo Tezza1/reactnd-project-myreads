ac2b6f1c4ef60ea80a219ca5b5e33b46296798dd
Tezza1 <tshenker@gmail.com>
2019-07-28 00:54:12 +0900

    func: added routing
    Added Routing for navigation to and from the searchpage

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,14 +1,12 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
-//import { Route } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const SearchButton = (props) => {
   return (
     <div className="open-search">
-      <button>
-        {/* <Link to={`/dialog/chat/${props.user}`}> */}
-        {/* </Link> */}
-      </button>
+      <Link to='/search'>
+        <button></button>
+      </Link>
     </div>
   )
 }
